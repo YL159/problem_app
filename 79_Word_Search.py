e5987d786f306c4102ecd427b0cdab53cca7f336
sea_melan <lan13931@gmail.com>
2024-01-10 23:33:19 -0500

    indent

--- 79_Word_Search.py
+++ 79_Word_Search.py
@@ -3,42 +3,42 @@ Leetcode 79 Word Search
 '''
 from typing import List
 class Solution:
-	def exist(self, board: List[List[str]], word: str) -> bool:
-		
-		def search(ws, i, j) -> bool:
-			# DFS from current (i,j) position
-			if not ws:
-				return True
-			# trim for valid neighbor coordinates
-			neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
-			for x, y in neighbors.copy():
-				if x < 0 or x >= m or y < 0 or y >= n:
-					neighbors.remove((x, y))
-			# get valid candidates for next recursion
-			cand = []
-			for x, y in neighbors:
-				if not visit[x][y] and board[x][y] == ws[0]:
-					cand.append((x, y))
-			# DFS with visite map back track
-			for x, y in cand:
-				visit[x][y] = 1
-				if search(ws[1:], x, y):
-					return True
-				visit[x][y] = 0
-			return False
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        
+        def search(ws, i, j) -> bool:
+            # DFS from current (i,j) position
+            if not ws:
+                return True
+            # trim for valid neighbor coordinates
+            neighbors = [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]
+            for x, y in neighbors.copy():
+                if x < 0 or x >= m or y < 0 or y >= n:
+                    neighbors.remove((x, y))
+            # get valid candidates for next recursion
+            cand = []
+            for x, y in neighbors:
+                if not visit[x][y] and board[x][y] == ws[0]:
+                    cand.append((x, y))
+            # DFS with visite map back track
+            for x, y in cand:
+                visit[x][y] = 1
+                if search(ws[1:], x, y):
+                    return True
+                visit[x][y] = 0
+            return False
 
-		m, n = len(board), len(board[0])
-		# maintain only 1 visit map, thus DFS
-		visit = [ [0 for i in range(n)] for j in range(m) ]
-		candidates = []
-		for i in range(m):
-			for j in range(n):
-				if board[i][j] == word[0]:
-					candidates.append((i, j))
-		for candidate in candidates:
-			i, j = candidate
-			visit[i][j] = 1
-			if search(word[1:], i, j):
-				return True
-			visit[i][j] = 0
-		return False
+        m, n = len(board), len(board[0])
+        # maintain only 1 visit map, thus DFS
+        visit = [ [0 for i in range(n)] for j in range(m) ]
+        candidates = []
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    candidates.append((i, j))
+        for candidate in candidates:
+            i, j = candidate
+            visit[i][j] = 1
+            if search(word[1:], i, j):
+                return True
+            visit[i][j] = 0
+        return False
--- ProcessLogs.py
+++ ProcessLogs.py
@@ -10,77 +10,77 @@ total CPU time of each process in the order of increasing PID.
 '''
 
 def logProcessing(n:int, logs:List[str]) -> List:
-	# record cumulative CPU time of each process
-	book = { i:0 for i in range(n)}
-	# process on the top of the stack is under processing
-	stack = []
-	prev = 0
-	for log in logs:
-		pid, event, t = log.split(':')
-		end = event == 'stop'
-		pid, t = int(pid), int(t)
-		# the process still occupies the ending time frame
-		if end:
-			t += 1
-		if not stack:
-			stack.append(pid)
-		elif pid == stack[-1]:
-			# this process must be ending now
-			book[pid] += t - prev
-			stack.pop()
-		else:
-			# a new process pre-empt the current process
-			book[stack[-1]] += t - prev
-			stack.append(pid)
-		prev = t
+    # record cumulative CPU time of each process
+    book = { i:0 for i in range(n)}
+    # process on the top of the stack is under processing
+    stack = []
+    prev = 0
+    for log in logs:
+        pid, event, t = log.split(':')
+        end = event == 'stop'
+        pid, t = int(pid), int(t)
+        # the process still occupies the ending time frame
+        if end:
+            t += 1
+        if not stack:
+            stack.append(pid)
+        elif pid == stack[-1]:
+            # this process must be ending now
+            book[pid] += t - prev
+            stack.pop()
+        else:
+            # a new process pre-empt the current process
+            book[stack[-1]] += t - prev
+            stack.append(pid)
+        prev = t
 
-	return [book[i] for i in range(n)]
+    return [book[i] for i in range(n)]
 
 def processLogsGen(n:int) -> None:
-	# Generate n random legal logs lists. Each represents 1-10 processes
-	ns = sorted([randint(1, 10) for i in range(n)])
-	for i in ns:
-		print(f'Number of prcesses: {i}')
-		logs = []
-		ts = sorted(sample(range(100), i*2))
-		for pid in range(i):
-			event = [f'{pid}:start:', f'{pid}:stop:']
-			index = randint(0, len(logs))
-			logs[index:index] = event
-		for j in range(len(ts)):
-			logs[j] = logs[j] + str(ts[j])
-		print(f'Logs: {logs}\n')
+    # Generate n random legal logs lists. Each represents 1-10 processes
+    ns = sorted([randint(1, 10) for i in range(n)])
+    for i in ns:
+        print(f'Number of prcesses: {i}')
+        logs = []
+        ts = sorted(sample(range(100), i*2))
+        for pid in range(i):
+            event = [f'{pid}:start:', f'{pid}:stop:']
+            index = randint(0, len(logs))
+            logs[index:index] = event
+        for j in range(len(ts)):
+            logs[j] = logs[j] + str(ts[j])
+        print(f'Logs: {logs}\n')
 
 if __name__ == '__main__':
-	processLogsGen(5)
+    processLogsGen(5)
 
 def test_case1():
-	n = 3
-	logs = ['0:start:0', '1:start:4', '1:stop:5', '2:start:7', '2:stop:10', '0:stop:11']
-	res = logProcessing(n, logs)
-	assert res == [6, 2, 4]
-	return
+    n = 3
+    logs = ['0:start:0', '1:start:4', '1:stop:5', '2:start:7', '2:stop:10', '0:stop:11']
+    res = logProcessing(n, logs)
+    assert res == [6, 2, 4]
+    return
 
 def test_case2():
-	n = 4
-	logs = ['0:start:15', '0:stop:32', '1:start:35', '3:start:37', '3:stop:45', '1:stop:79', '2:start:81', '2:stop:93']
-	res = logProcessing(n, logs)
-	assert res == [18, 36, 13, 9]
-	return
+    n = 4
+    logs = ['0:start:15', '0:stop:32', '1:start:35', '3:start:37', '3:stop:45', '1:stop:79', '2:start:81', '2:stop:93']
+    res = logProcessing(n, logs)
+    assert res == [18, 36, 13, 9]
+    return
 
 def test_case3():
-	n = 6
-	logs = ['2:start:12', '2:stop:27', '3:start:35', '3:stop:36', '0:start:52', '0:stop:53', '1:start:66', '4:start:68', \
-		 	'4:stop:71', '5:start:74', '5:stop:84', '1:stop:90']
-	res = logProcessing(n, logs)
-	assert res == [2, 10, 16, 2, 4, 11]
-	return
+    n = 6
+    logs = ['2:start:12', '2:stop:27', '3:start:35', '3:stop:36', '0:start:52', '0:stop:53', '1:start:66', '4:start:68', \
+             '4:stop:71', '5:start:74', '5:stop:84', '1:stop:90']
+    res = logProcessing(n, logs)
+    assert res == [2, 10, 16, 2, 4, 11]
+    return
 
 def test_case4():
-	n = 10
-	logs = ['0:start:2', '5:start:18', '7:start:22', '7:stop:23', '6:start:27', '9:start:28', '9:stop:32', \
-		 	'8:start:33', '8:stop:43', '6:stop:48', '5:stop:49', '4:start:54', '4:stop:55', '2:start:59', '2:stop:70', \
-			'1:start:75', '1:stop:86', '3:start:90', '3:stop:94', '0:stop:96']
-	res = logProcessing(n, logs)
-	assert res == [32, 12, 12, 5, 2, 8, 6, 2, 11, 5]
-	return
+    n = 10
+    logs = ['0:start:2', '5:start:18', '7:start:22', '7:stop:23', '6:start:27', '9:start:28', '9:stop:32', \
+             '8:start:33', '8:stop:43', '6:stop:48', '5:stop:49', '4:start:54', '4:stop:55', '2:start:59', '2:stop:70', \
+            '1:start:75', '1:stop:86', '3:start:90', '3:stop:94', '0:stop:96']
+    res = logProcessing(n, logs)
+    assert res == [32, 12, 12, 5, 2, 8, 6, 2, 11, 5]
+    return
